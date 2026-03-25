c49bd9a240e2c7025abed72f36d790291241a7e4
cbn0003 <cbn0003@alu.medac.es>
2026-03-25 09:58:50 +0100

    Renombrada la carpeta services por repository, con sus correspondientes cambios en imports.

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/product_model.dart';
-import '../services/product_service.dart';
+import '../repository/product_service.dart';
 
 class ProductProvider extends ChangeNotifier {
   final ProductService _service = ProductService();
